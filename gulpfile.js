e1029abfd866e8be44e76bcbb35582b868ea33b4
Manikandan <manikandan.meraki@gmail.com>
2018-07-05 08:35:30 +0530

    tenni website

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,28 @@
-var gulp = require('gulp');
+var gulp = require('gulp'), imagemin = require('gulp-imagemin'),
+cssmin = require('gulp-cssmin'), rename = require('gulp-rename'),concat = require('gulp-concat');
+ 
+
+gulp.task('images', () =>
+    gulp.src('./images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
+
+ 
+gulp.task('concat', function() {
+  return gulp.src(['./css/**/*.css')
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('./css/all/'));
+});
+ 
+gulp.task('cssmin', function () {
+    gulp.src('css/all/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css'));
+});
+
+
+
+
+gulp.task('default', ['images','cssmin','concat']);
